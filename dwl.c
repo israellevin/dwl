28f33e48fc2681405a2c134c1e14a3625d2ccf1c
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-08-08 14:33:03 -0600

    improve checking in configurex11()
    this avoids a client resizing itself when the user is interactively resizing
    the client

--- dwl.c
+++ dwl.c
@@ -3645,7 +3645,7 @@ configurex11(struct wl_listener *listener, void *data)
 				event->x, event->y, event->width, event->height);
 		return;
 	}
-	if (c->isfloating)
+	if ((c->isfloating && c != grabc) || !c->mon->lt[c->mon->sellt]->arrange)
 		resize(c, (struct wlr_box){.x = event->x, .y = event->y,
 				.width = event->width + c->bw * 2, .height = event->height + c->bw * 2}, 0);
 	else
